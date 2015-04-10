8f5b811c884590cb0dacefaf9ce4d675dab5ed48
mateus <mateus@mateus-VPCSB35FB>
2015-04-10 19:10:44 -0300

    Adição DAO

---
+++ src/main/java/br/unifor/pin/saa/dao/CursaDAO.java
@@ -0,0 +1,34 @@
+package br.unifor.pin.saa.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.unifor.pin.saa.entity.Cursa;
+
+@Repository
+@Transactional(propagation=Propagation.REQUIRED)
+public class CursaDAO {
+
+	@PersistenceContext//O "@PersistenceContext" é responsável por injetar um objeto dentro do entityManager
+	private EntityManager entityManager;
+		
+	public void salvar(Cursa cursa) {//Incluido metodo salvar.
+		entityManager.persist(cursa);
+	}
+	public void atualizar(Cursa cursa) {//Incluido metodo atualizar.
+		entityManager.merge(cursa);
+	}	
+
+	public void excluir(Cursa cursa) {//Incluido metodo excluir.
+		entityManager.remove(cursa);
+	}
+	
+	public Cursa buscarPorId(Long id){//Incluido metodo buscar.
+		return entityManager.find(Cursa.class, id);
+	
+    }
+}
