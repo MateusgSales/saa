59b9dd589d6ddf2faf8dd84253712b699b66918e
bruno-ana <bruno.costa.1985@gmail.com>
2015-05-04 20:54:50 -0300

    //teste primeiro commit. não foi feita nenhuma modificação no codigo

--- src/main/java/br/unifor/pin/saa/dao/AlunosDAO.java
+++ src/main/java/br/unifor/pin/saa/dao/AlunosDAO.java
@@ -1,10 +1,9 @@
 package br.unifor.pin.saa.dao;
 
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
--- src/test/java/br/unifor/pin/saa/dao/TurmasDAOTest.java
+++ src/test/java/br/unifor/pin/saa/dao/TurmasDAOTest.java
@@ -1,6 +1,5 @@
 package br.unifor.pin.saa.dao;
 
-import static org.junit.Assert.*;
 import junit.framework.Assert;
 
 import org.junit.Test;
