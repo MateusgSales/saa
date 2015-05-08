dd68c254e8ce3ca36705396d9c5d5c053015f2fc
bruno-ana <bruno.costa.1985@gmail.com>
2015-05-08 18:00:17 -0300

    [walter bruno] Conclusão dos testes

---
+++ src/test/java/br/unifor/pin/saa/dao/Suit_testes_DAO.java
@@ -0,0 +1,13 @@
+package br.unifor.pin.saa.dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AlunosDAOTest.class, AulasDAOTest.class,
+		AvaliacoesDAOTest.class, CursaDaoTest.class, InstituicoesDAOTest.class,
+		ProfessoresDAOTest.class, TurmasDAOTest.class, UsuarioDAOTest.class })
+public class Suit_testes_DAO {
+
+}
